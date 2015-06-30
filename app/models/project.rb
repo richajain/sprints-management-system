1ab8186af7bc6577032421007b5ecbffbe74db84
Richa Jain <richajain@Richas-MacBook-Pro.local>
2015-06-30 18:48:18 +0530

    added users controller

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,5 @@ class Project < ActiveRecord::Base
   has_many :works
   has_many :users, through: :works 
   has_one :manager, class_name: "User"
+  has_many :tasks
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_many :works
-  has_many :projects, through: :works 
+  has_many :projects, through: :works
+  
+  def self.search(query)
+  	where("email like ?", "%#{query}%") 
+  end
+  
 end
