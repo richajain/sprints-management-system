444a317f529c4ff4051466bce9514a9507c00cd8
Abdul-Khalid <abdkhalid9987@gmail.com>
2015-07-02 17:06:58 +0530

    landing page done

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root :to => 'project#new'
-
+  # root :to => 'project#new'
+  root :to => 'welcome#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
